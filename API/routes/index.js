ddb7a6468d76f14364ce222c5db89ca9047f6005
AJ Danelz <andrew.danelz@m33integrated.com>
2015-08-29 04:48:43 -0400

    just a lil swag

--- API/routes/index.js
+++ API/routes/index.js
@@ -5,32 +5,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 /* GET Clubs. */
 router.get('/club/list', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  Club.find(function(err, clubs){
-    res.send(JSON.stringify(clubs));
-  });
+    res.setHeader('Content-Type', 'application/json');
+    Club.find(function(err, clubs){
+        res.send(JSON.stringify(clubs));
+    });
 });
 /* GET Club by id */
 router.get('/club/:id', function(req, res) {
-  var _id = req.params.id;
+    var _id = req.params.id;
 
-  res.setHeader('Content-Type', 'application/json');
-  Club.findById(_id,function(err, club){
-    res.send(JSON.stringify(club));
-  });
+    res.setHeader('Content-Type', 'application/json');
+    Club.findById(_id,function(err, club){
+        res.send(JSON.stringify(club));
+    });
 });
 /* GET Club by owner */
 router.get('/clubsByOwner/:id', function(req, res) {
-  var ownerId = req.params.id;
+    var ownerId = req.params.id;
 
-  res.setHeader('Content-Type', 'application/json');
-  Club.find({ownerId: ownerId},function(err, clubs){
-    res.send(JSON.stringify(clubs));
-  });
+    res.setHeader('Content-Type', 'application/json');
+    Club.find({ownerId: ownerId},function(err, clubs){
+        res.send(JSON.stringify(clubs));
+    });
 });
 
 
@@ -38,28 +38,33 @@ router.get('/clubsByOwner/:id', function(req, res) {
 
 /* POST Club page. */
 router.post('/club/save', function(req, res) {
-  var jsonClub = req.body;
-  if(jsonClub._id){
-    Club.findByIdAndUpdate(jsonClub._id,jsonClub,function(err, club){
-      if (err) {
-        res.send("There was a problem updating the information to the database: " + err);
-      }else{
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(club));
-      }
-    });
-  }else{
-    res.setHeader('Content-Type', 'application/json');
-    var club = new Club(jsonClub);
-    club.save(function(err){
-      if(err){
-        res.send(JSON.stringify({success:false}))
-      }else{
-        res.send(JSON.stringify(club));
-      }
-    });
+    var jsonClub = req.body;
 
-  }
+    if(!jsonClub){
+        res.send("send in an object dummy");
+    }
+
+    if(jsonClub._id){
+        Club.findByIdAndUpdate(jsonClub._id,jsonClub,function(err, club){
+            if (err) {
+                res.send("There was a problem updating the information to the database: " + err);
+            }else{
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(club));
+            }
+        });
+    }else{
+        res.setHeader('Content-Type', 'application/json');
+        var club = new Club(jsonClub);
+        club.save(function(err){
+            if(err){
+                res.send(JSON.stringify({success:false}))
+            }else{
+                res.send(JSON.stringify(club));
+            }
+        });
+
+    }
 
 
 });
@@ -68,35 +73,38 @@ router.post('/club/save', function(req, res) {
 
 /* register for club Club page. */
 router.post('/club/register/:id', function(req, res) {
-  var _id = req.params.id;
-  var jsonUser = req.body;
-  if(_id){
-    Club.findByIdAndUpdate(_id, {$pull: {'members': {id:jsonUser.id}}}, {new: true}, function (err, club) {
-      if (err) { return res.send(err) }
+    var _id = req.params.id;
+    var jsonUser = req.body;
+    console.log("to register: " + JSON.stringify(jsonUser));
+    if(jsonUser && _id){
+        Club.findByIdAndUpdate(_id, {$pull: {'members': {id:jsonUser.id}}}, {new: true}, function (err, club) {
+            if (err) { return res.send(err) }
 
-      Club.findByIdAndUpdate(_id,{$push: {members: jsonUser}},function(err, club){
-        if (err) {
-          res.send("There was a problem updating the information to the database: " + err);
-        }else{
-          res.setHeader('Content-Type', 'application/json');
-          res.send(JSON.stringify(club));
-        }
-      });
-    });
+            Club.findByIdAndUpdate(_id,{$push: {members: jsonUser}},function(err, club){
+                if (err) {
+                    res.send("There was a problem updating the information to the database: " + err);
+                }else{
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify(club));
+                }
+            });
+        });
 
-  }
+    }
 });
 
 
 /* register for club Club page. */
 router.get('/clubsByRegistered/:id', function(req, res) {
 
-  var registeredId = req.params.id;
+    var registeredId = req.params.id;
+    if(registeredId){
+        res.setHeader('Content-Type', 'application/json');
+        Club.find({"members.id": registeredId.toString()},function(err, clubs){
+            res.send(JSON.stringify(clubs));
+        });
+    }
 
-  res.setHeader('Content-Type', 'application/json');
-  Club.find({"members.id": registeredId.toString()},function(err, clubs){
-    res.send(JSON.stringify(clubs));
-  });
 });
 
 
